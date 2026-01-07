7f942defb5ade75e20397f6553e4aab3d53754bc
panchal Dnyaneshwar <panchaldnyaneshwar.m@gmail.com>
2026-01-07 07:03:47 +0530

    Basics

---
+++ Demo2.java
@@ -0,0 +1,31 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        
+        float f = 45.1f;
+        int g = 47;
+
+        // if (f>40 && g>40) 
+        if(f<40 && g>40) {
+            System.out.println("Passed");
+        }
+        else{
+            System.out.println("Failed");
+        }
+
+
+        // if(f>40 || g>40)
+         if(f<40 || g<40){
+             System.out.println("Passed");
+        }
+        else{
+            System.out.println("Failed");
+        }
+        
+
+    }
+}
+
+/*
+    Use of logical oprators 
+    if-else and if - else if -else too.
+*/
---
+++ TerneryOp.java
@@ -0,0 +1,19 @@
+public class TerneryOp {
+    public static void main(String[] args) {
+        
+        int num = 4;
+        int result = 0;
+
+
+        if (num % 2 == 0) 
+            result = 10;
+        else
+            result = 20;
+
+        System.out.println(result);
+    }
+}
+
+/*
+    
+*/
